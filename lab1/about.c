d382e3f8e04ff71ec40b229cb418b6a98694d457
Andrew Rothman <asrothman1@usfca.edu>
2025-01-30 11:10:06 -0800

    added about file

---
+++ lab1/about.c
@@ -0,0 +1,27 @@
+/*
+Hello World!
+------------
+Author: Andrew Rothman
+USF Username: asrothman1
+Preferred Name: Andrew
+Pronouns: he/him/his
+About me:
+    San Francisco Native.  Some would say Indiana Jones meets Bruce Springsteen.
+    */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello world!\n");
+    printf("------------\n"); 
+    printf("Author: Andrew Rothman\n");
+    printf("USF Username: asrothman1\n");
+    printf("Preferred Name: Andrew\n");
+    printf("Pronouns: he/him/his\n");
+    printf("About me:\n");
+    printf("       San Francisco Native.\n");
+    printf("       Some would say Indiana Jones meets Bruce Springsteen.\n");
+
+    return 0;
+}
